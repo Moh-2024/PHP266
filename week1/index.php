0c6b081e93aa2ec094060bbc99673722a49f2b77
Moh-2024 <mmalek@email.neit.edu>
2024-10-08 11:40:19 -0400

    week2 update

---
+++ week1/index.php
@@ -0,0 +1,5 @@
+<?php include '../includes/header.php'; ?>
+<h1>Week 1 Assignments</h1>
+<p>Details of Week 1 assignments go here.</p>
+<a href="../index.php">Back to Portfolio</a>
+<?php include '../includes/footer.php'; ?>
